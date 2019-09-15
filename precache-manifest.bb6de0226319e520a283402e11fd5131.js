a10b07c0cf392f93fe1d6a3eb70aee6034804e11
G0TT0N <gotton92@mail.ru>
2019-09-15 20:11:02 +0300

    Updates

---
+++ precache-manifest.bb6de0226319e520a283402e11fd5131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0cda45c211eacbafd7d071decfb01e",
+    "url": "/temperature-converter/index.html"
+  },
+  {
+    "revision": "2403328ad240a2babf4f",
+    "url": "/temperature-converter/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1d99c9b04f0c6ef199bd",
+    "url": "/temperature-converter/static/js/2.12cbd233.chunk.js"
+  },
+  {
+    "revision": "2403328ad240a2babf4f",
+    "url": "/temperature-converter/static/js/main.45cb18d3.chunk.js"
+  },
+  {
+    "revision": "f90e5eed9cc1194b6976",
+    "url": "/temperature-converter/static/js/runtime~main.64512143.js"
+  }
+]);
